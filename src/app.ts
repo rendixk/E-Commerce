0b73e88259c6d52b3cc577e6f5a9f5eeaf60b92a
rendixk <ferendfio@gmail.com>
2025-08-28 16:26:58 +0700

    Initial Commit

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import opening from './route/opening.js'
 import authRoute from './route/authRoute.js'
 import userAuth from './route/userRoute.js'
@@ -7,6 +8,8 @@ import productRouter from './route/productRoute.js'
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
 
 app.use('/', opening)
 app.use('/auth', authRoute)
