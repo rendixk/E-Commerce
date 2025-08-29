1aad1eaf5f615fbd746eec35c07cb8db73c032d8
rendixk <ferendfio@gmail.com>
2025-08-29 09:51:54 +0700

    Initial Commit

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ import productRouter from './route/productRoute.js'
 
 const app = express()
 const corsOption = {
-   origin: 'http://localhost:5173'
+   origin: 'http://localhost:5173',
+   methods: ["GET", "POST", "PUT", "DELETE"],
+   allowHeaders: ["Content-Type", "Authorization"],
+   credentials: true
 }
 
 app.use(express.json())
