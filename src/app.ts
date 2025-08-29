af723b99a6e45db6c8fb4dcc63d6b6bea4ec8f07
rendixk <ferendfio@gmail.com>
2025-08-29 13:14:10 +0700

    Initial Commit

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,17 @@ import userAuth from './route/userRoute.js'
 import productRouter from './route/productRoute.js'
 
 const app = express()
-const corsOption = {
-   origin: 'http://localhost:5173',
-   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-   allowedHeaders: ['Content-Type', 'Authorization'],
-   credentials: true,
-   optionsSuccessStatus: 200
- };
+// const corsOption = {
+//    origin: 'http://localhost:5173',
+//    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//    allowedHeaders: ['Content-Type', 'Authorization'],
+//    credentials: true,
+//    optionsSuccessStatus: 200
+//  };
 
 app.use(express.json())
-app.use(cors(corsOption))
+// app.use(cors(corsOption))
+app.use(cors())
 
 app.use('/', opening)
 app.use('/auth', authRoute)
