35e727e6c7da30a583aa6db57d8fb104ec08eb0f
rendixk <ferendfio@gmail.com>
2025-09-04 22:56:49 +0700

    chore: remove unused temporary files

--- src/route/utilsRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { clearDatabase } from '../controller/utilsController.js'
-
-const router = Router()
-
-router.delete('/cleardb', clearDatabase)
-
-export default router
