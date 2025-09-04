5e79d2ee5badd263a261c55d103c5ee19698faa9
rendixk <ferendfio@gmail.com>
2025-09-04 22:27:26 +0700

    Initial Commit

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,24 +1,25 @@
 import type { Request, Response } from 'express'
 import { prisma } from '../prisma.js'
 import type { AuthRequest } from '../middleware/authMiddleware.js'
+import chalk from 'chalk';
 
 //create profile user
 export const createProfile = async (req: AuthRequest, res: Response) => {
-   console.log("Creating new user profile...");
-   const userId = req.user?.id;
-   const userEmail = req.user?.email;
-   const { fullname, address } = req.body;
+   console.log(chalk.cyan("Creating new user profile..."))
+   const userId = req.user?.id
+   const userEmail = req.user?.email
+   const { fullname, address } = req.body
  
    if (!userId || !userEmail) {
-     return res.status(401).json({ message: "Unauthorized." });
+     return res.status(401).json({ message: "Unauthorized." })
    }
    try {
-     const existingProfile = await prisma.profiles.findUnique({
+     const existingProfile = await prisma.profiles.findFirst({
        where: { user_id: userId },
      });
  
       if (existingProfile) {
-         console.log("Profile already exists.");
+         console.log(chalk.yellow("Profile already exists."))
          return res.status(409).json({ message: "Profile already exists." });
       }
       const newProfile = await prisma.profiles.create({
@@ -30,19 +31,19 @@ export const createProfile = async (req: AuthRequest, res: Response) => {
          },
       });
  
-     console.log("Profile created successfully.");
-     res.status(201).json({ message: "Profile created successfully!", newProfile });
+      console.log(chalk.green("Profile created successfully."))
+      res.status(201).json({ message: "Profile created successfully!", newProfile })
    } 
    catch (error) {
-     console.error(`Failed to create profile: ${error}`);
-     res.status(500).json({ message: "Something went wrong" });
+      console.error(chalk.red(`Failed to create profile: ${error}`))
+     res.status(500).json({ message: "Something went wrong" })
    }
- };
+ }
  
 
 //get profile user
 export const getProfile = async (req: AuthRequest, res: Response) => {
-   console.log("Fetching user profile...")
+   console.log(chalk.cyan("Fetching user profile..."))
    const userId = req.user?.id
 
    if(!userId) {
@@ -56,22 +57,22 @@ export const getProfile = async (req: AuthRequest, res: Response) => {
       })
 
       if(!profile) {
-         console.log("Profile not found for this user.")
+         console.log(chalk.yellow("Profile not found for this user."))
          return res.status(404).json({ message: "Profile not found." })
       }
 
-      console.log("User profile fetcehd successfully.")
+      console.log(chalk.green("User profile fetched successfully."))
       res.status(200).json(profile)
    } 
    catch (error) {
-      console.error(`Failed to fetch profile: ${error}`)
+      console.error(chalk.red(`Failed to fetch profile: ${error}`))
       res.status(500).json({ message: "Something went wrong" })
    }
 }
 
 //update useer profile
 export const updateProfile = async (req: AuthRequest, res: Response) => {
-   console.log("Updating user profile...")
+   console.log(chalk.cyan("Fetching user profile..."))
    const userId = req.user?.id
    const { fullname, address } = req.body
 
@@ -82,7 +83,7 @@ export const updateProfile = async (req: AuthRequest, res: Response) => {
    try {
       const existProfile = await prisma.profiles.findUnique({ where: { user_id: userId} })
       if(!existProfile) {
-         console.log("Update failed: Profile not foun for this user")
+         console.log(chalk.yellow("Update failed: Profile not found for this user."))
          return res.status(404).json({ message: "Profile not found: cannot update." })
       }
 
@@ -93,11 +94,11 @@ export const updateProfile = async (req: AuthRequest, res: Response) => {
             address
          }
       })
-      console.log("Profile updated successfully.")
+      console.log(chalk.green("Profile updated successfully."))
       res.status(201).json({ message: "Profile updated successfully", updatedProfile })
    }
    catch (error) {
-      console.error(`Failed to update profile: ${error}`)
+      console.error(chalk.red(`Failed to update profile: ${error}`))
       res.status(500).json({ message: "Something went wrong" })
    }
 }
@@ -105,7 +106,7 @@ export const updateProfile = async (req: AuthRequest, res: Response) => {
 
 //user balance 
 export const getBalance = async (req: AuthRequest, res: Response) => {
-   console.log("Fetching user balance...")
+   console.log(chalk.cyan("Fetching user balance..."))
    const userId = req.user?.id
 
    if(!userId) {
@@ -118,7 +119,7 @@ export const getBalance = async (req: AuthRequest, res: Response) => {
          select: { amount: true}
       })
       if(!balance) {
-         console.log("Balance not found for this user. Creating default balance")
+         console.log(chalk.yellow("Balance not found for this user. Creating default balance."))
          const newBalance = await prisma.balance.create({
             data: {
                user_id: userId,
@@ -128,18 +129,18 @@ export const getBalance = async (req: AuthRequest, res: Response) => {
          return res.status(200).json({ amount: newBalance.amount })
       }
 
-      console.log("Balance fetched successfully")
+      console.log(chalk.green("Balance fetched successfully."))
       res.status(200).json({ amount: balance.amount })
    } 
    catch (error) {
-      console.error(`Failed to fetch balance: ${error}`)
+      console.error(chalk.red(`Failed to fetch balance: ${error}`))
       res.status(500).json({ message: "Something went wrong" })
    }
 }
 
 //topup buyer balance 
 export const topupBalance = async (req: AuthRequest, res: Response) => {
-   console.log("User is topping up balance...")
+   console.log(chalk.cyan("User is topping up balance..."))
    const userId = req.user?.id
    const { amount } = req.body
    if (!userId) {
@@ -153,7 +154,7 @@ export const topupBalance = async (req: AuthRequest, res: Response) => {
       const existBalance = await prisma.balance.findUnique({ where: { user_id: userId } })
       let updatedBalance
       if(!existBalance) {
-         console.log("Balance record not found. Creating one... ")
+         console.log(chalk.yellow("Balance record not found. Creating one... "))
          updatedBalance = await prisma.balance.create({
             data: {
                user_id: userId,
@@ -176,11 +177,11 @@ export const topupBalance = async (req: AuthRequest, res: Response) => {
          }
       })
 
-      console.log("Top-up successful")
+      console.log(chalk.green("Top-up successful."))
       res.status(200).json({ message: "Top-up successful", balance: updatedBalance})
    } 
    catch (error) {
-      console.error(`top-up failed: ${error}`),
+      console.error(chalk.red(`top-up failed: ${error}`))
       res.status(500).json({ message: "Something went wrong" })
    }
 }
