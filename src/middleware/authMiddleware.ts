2605d5e464f5bdb18b6b5b64d993181f5d2e480b
rendixk <ferendfio@gmail.com>
2025-09-04 19:23:05 +0700

    Initial Commit

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -13,21 +13,21 @@ export interface AuthRequest extends Request {
 }
 
 export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
 
   if (token == null) {
-    console.log('Authentication failed: Token missing.');
-    return res.status(401).json({ message: 'Authentication failed. No token provided.' });
+    console.log('Authentication failed: Token missing.')
+    return res.status(401).json({ message: 'Authentication failed. No token provided.' })
   }
 
   jwt.verify(token, JWT_SECRET, (err: any, user: any) => {
     if (err) {
-      console.log('Authentication failed: Invalid token.');
-      return res.status(403).json({ message: 'Authentication failed. Invalid token.' });
+      console.log('Authentication failed: Invalid token.')
+      return res.status(403).json({ message: 'Authentication failed. Invalid token.' })
     }
     
-    req.user = user;
-    next();
-  });
-};
+    req.user = user
+    next()
+  })
+}
