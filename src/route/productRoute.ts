d6644856a09648913d4fb663e932b47b40b62f69
rendixk <ferendfio@gmail.com>
2025-09-17 09:34:42 +0700

    Initial Commit

--- src/route/productRoute.ts
+++ src/route/productRoute.ts
@@ -1,17 +1,18 @@
 import { Router} from 'express'
 import { authenticateToken } from '../middleware/authMiddleware.js'
 import upload from '../middleware/uploadMiddleware.js'
-import { createProduct, getAllProduct, getProductById, updateProduct, deleteProduct } from '../controller/productController.js'
+import { createProduct, getAllProduct, searchProduct, getProductById, updateProduct, deleteProduct } from '../controller/productController.js'
 
 const router = Router()
 
 // route to manage the product (only for seller)
-router.post('/add', authenticateToken, upload.single('image'), createProduct);
-router.put('/:id', authenticateToken, upload.single('image'), updateProduct);
-router.delete('/:id', authenticateToken, deleteProduct);
+router.post('/add', authenticateToken, upload.single('image'), createProduct)
+router.put('/:id', authenticateToken, upload.single('image'), updateProduct)
+router.delete('/:id', authenticateToken, deleteProduct)
 
 // Public Router (for all user)
-router.get('/', getAllProduct);
-router.get('/:id', getProductById);
+router.get('/', getAllProduct)
+router.get('/', searchProduct)
+router.get('/:id', getProductById)
 
 export default router
