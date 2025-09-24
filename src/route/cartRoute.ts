a8868381fd796cb74ab71861113b81ba08edc42a
rendixk <ferendfio@gmail.com>
2025-09-24 12:33:23 +0700

    Add: Edit cart item quantity

--- src/route/cartRoute.ts
+++ src/route/cartRoute.ts
@@ -1,6 +1,6 @@
 import { Router} from 'express'
 import { authenticateToken } from '../middleware/authMiddleware.js'
-import { addToCart, getCart, deleteCartItem } from '../controller/cartController.js'
+import { addToCart, getCart, editCart, deleteCartItem } from '../controller/cartController.js'
 
 const router = Router()
 
@@ -9,6 +9,9 @@ router.post('/add', authenticateToken, addToCart)
 
 //router to view all item inside cart
 router.get('/', authenticateToken, getCart)
+
+//router to edit item quantity
+router.put('/edit/:id', authenticateToken, editCart)
 
 //router to delete an item from cart
 router.delete('/:id', authenticateToken, deleteCartItem)
